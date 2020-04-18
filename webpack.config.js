7c1be6e5300509362ea263b1851cc2e2d1928e19
Bassit <barseetbrown@gmail.com>
2020-04-18 18:27:04 +0100

    set up production server with express

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
-
-
-module.exports = {
-    entry : './src/app.js',
+module.exports =(env) => {
+    const isProduction = env === 'production'
+    console.log('env', env)
+    return{
+        entry : './src/app.js',
     output : {
         path: path.join(__dirname, 'public'),
         filename : 'bundle.js'
@@ -22,8 +23,10 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction?'source-map':'cheap-module-eval-source-map',
     devServer : {
         contentBase : path.join(__dirname, 'public')
     }
+    }
+    
 }
